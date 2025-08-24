01674cc98c1643c8a899f7a4e149ce8c6b74dc21
CyberHasH <b67gary@gmail.com>
2025-08-24 17:06:57 +0800

    Add fit 4g to interface candidate

--- opendbc/car/honda/values.py
+++ opendbc/car/honda/values.py
@@ -237,7 +237,7 @@ class CAR(Platforms):
   )
   HONDA_FIT_4G = HondaBoschPlatformConfig(
     [
-      HondaCarDocs("Honda Fit e:Hev (Taiwan) 2022", "All"),
+      HondaCarDocs("Honda Fit (Taiwan) 2022", "All"),
     ],
     CarSpecs(mass=2867 * CV.LB_TO_KG, wheelbase=2.7, steerRatio=13.0, centerToFrontRatio=0.41),
     {Bus.pt: 'honda_civic_ex_2022_can_generated'},
